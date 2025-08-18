038c8ab810dfa130289921670fcc2b75e67da8da
email2ify <110549305+email2ify@users.noreply.github.com>
2025-08-18 12:06:10 +0200

    sign in and out initials

--- my-app/src/EventNesting.js
+++ my-app/src/EventNesting.js
@@ -10,25 +10,40 @@ class EventNesting extends Component {
         super(props);
         this.state = {
             //isLoading: true,
-            isLoggedIn: true,
+            isLoggedIn: true
+            
         }
     }
 
-        handleSignIn = () => { this.setState({isLoggedIn : true})}
-        handleSignOut = () => { this.setState({isLoggedOut : false})}
+    handleSignIn = () => { this.setState({isLoggedIn : true})
+    }
+    handleSignOut = () => { this.setState({isLoggedIn : false})
+    }
 
 
     render() {
         return (
-            <div>
+        <div>
 
+            {
+                this.state.isLoggedIn ? (
+                    <div>
+                        <p> Welcome to the site! Please complete the steps below</p>
+                        <ol>
+                            <li>confirm email</li>
+                            <li>complete profile</li>
+                            <li>Subscribe to the Newsletter</li>
+                        </ol>
+                        <button onClick={this.handleSignOut }>Sign Out</button>
+                    </div>
+                ) : (
+                    <div>
+                        <button onClick={this.handleSignIn }>Sign In</button>
+                        <p>Welcome Back</p>
+                    </div>)
+            }
 
-                {/* < UserData isLoading={this.state.isLoading} /> */}
-                <h1>{this.state.isLoggedIn ? 'Welcome to Neuro-Blockchain!': 'Welcome to Neuro-Blockchain'}</h1>
-                <UserMessage isLoggedIn={this.state.isLoggedIn} />
-                <UserMessage isLoggedOut={this.state.isLoggedOut} />
-
-            </div>
+        </div>
 
         )
 
